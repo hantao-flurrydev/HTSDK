f6583c82489a645484ca6d8974fdc5e79b24a10b
Hantao Yang <hantao.yang@verizonmedia.com>
2020-11-30 12:12:33 -0800

    first sdk

---
+++ CalculateSDK/CalculateSDK.h
@@ -0,0 +1,16 @@
+//
+//  CalculateSDK.h
+//  CalculateSDK
+//
+//  Created by Hantao Yang on 11/25/20.
+//  Copyright © 2020 Hantao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculateSDK : NSObject
+
++ (NSInteger)calculate:(NSInteger) num1 add:(NSInteger)num2;
+
++ (NSInteger)calculate:(NSInteger) num1 minus:(NSInteger)num2;
+@end
